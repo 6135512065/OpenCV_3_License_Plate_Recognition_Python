b98299323b65131abf43ce37a116d4604e230965
6135512065 <lan_6744@hotmail.com>
2021-04-25 01:50:51 +0700

    first commit

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ def main():
         return                                                          # and exit program
     # end if
 
-    imgOriginalScene  = cv2.imread("ImagesCar/4.jpg")               # open image
+    imgOriginalScene  = cv2.imread("ImagesCar/Test4.jpg")               # open image
 
     if imgOriginalScene is None:                            # if image was not read successfully
         print("\nerror: image not read from file \n\n")  # print error message to std out
